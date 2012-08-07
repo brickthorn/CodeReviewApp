35e08e37aaf097a9df8536d5dd9ce0d6a2e62be7
Damien <damien.kan@gmail.com>
2012-08-07 10:55:22 -0700

    header fixed position

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(:version => 20120806232850) do
+ActiveRecord::Schema.define(:version => 20120807000948) do
 
   create_table "code_submissions", :force => true do |t|
     t.integer  "user_id"
@@ -38,8 +37,6 @@ ActiveRecord::Schema.define(:version => 20120806232850) do
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
   end
-=======
-ActiveRecord::Schema.define(:version => 20120807000948) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -59,6 +56,5 @@ ActiveRecord::Schema.define(:version => 20120807000948) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
->>>>>>> master
 
 end
