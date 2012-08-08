fc7486a4eb0fa587bf090d98687b6c3a77951725
Apprentice <apprentice@devbootcamp.com>
2012-08-07 17:44:51 -0700

    show and new views w testing

--- app/controllers/code_submissions_controller.rb
+++ app/controllers/code_submissions_controller.rb
@@ -5,15 +5,24 @@ class CodeSubmissionsController < ApplicationController
   end
 
   def new
+    @code_submission = CodeSubmission.new
   end
 
   def create
+    @code_submission = CodeSubmission.new(params[:code_submission])
+    if @code_submission.save
+      flash[:success] = "You successfully submitted your code for review!"
+      redirect_to code_submission_path(@code_submission)
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
   end
 
   def destroy
   end
 
   def show
-  	raise
+  	@code_submission = CodeSubmission.find(params[:id])
   end
 end
--- spec/requests/code_submission_pages_spec.rb
+++ spec/requests/code_submission_pages_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe "code submission pages" do
   subject { page }
-  describe "resolves to index page of code submissions for root" do
-
-    before(:each) do  
+  describe "index page for code submissions" do
+    before :each do  
       @code_submission1 = Fabricate(:code_submission)
       @code_submission2 = Fabricate(:code_submission)
       @code_submission3 = Fabricate(:code_submission)
@@ -22,13 +21,27 @@ describe "code submission pages" do
     it "should go to the new code submission page when clicking the button" do
       click_link("Submit Your Code")
       current_path.should == new_code_submission_path
-      page.should have_content("Enter Code Url")
-      
+      page.should have_content("Enter code url")
     end
     
-
+    describe "submission page" do
+      before :each do
+        visit new_code_submission_path 
+      end
+      
+      it "should go to the #new page after clicking Submit Link and take in form data to show correctly" do
+        page.should have_field("code_submission_url")
+        new_id = CodeSubmission.last.id + 1
+        page.fill_in 'code_submission_url', :with => "http://www.github.com/test/"
+        page.fill_in 'code_submission_description', :with => "lorem ipsum veritaie huckster"
+        page.click_button 'Submit'
+        current_path.should == code_submission_path(new_id)
+        page.should have_content("http://www.github.com/test/")
+      end
+    
+    
+    end
     
   end
-  
 end
 
