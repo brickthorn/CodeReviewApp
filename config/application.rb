88cd56a48c84f1ed86e7a3fadb4d288ee5ec03d1
dbc-apprentice <apprentice@devbootcamp.com>
2012-08-08 17:15:01 -0700

    views, things look better

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module CodeReviewApp
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += %W(#{config.root}/vendor/assets)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
@@ -58,9 +59,9 @@ module CodeReviewApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    
+
     # Needed for Devise
     config.assets.initialize_on_precompile = false
-    
+
   end
 end
